6f2930bfe98d50db3607d960d3e61104f7b11b24
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-01-24 22:00:05 +0900

    update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import { logRoles, render, screen } from '@testing-library/react'
+import {
+  fireEvent,
+  // logRoles,
+  render,
+  screen,
+} from '@testing-library/react'
 import App from './App'
 
 test('button has correct initial color', () => {
-  const { container } = render(<App />)
-  logRoles(container)
+  // const { container } =
+  render(<App />)
+  // logRoles(container)
   const colorButton = screen.getByRole('button', { name: 'Change to blue' })
 
   expect(colorButton).toHaveStyle({ 'background-color': 'red' })
+
+  fireEvent.click(colorButton)
+
+  expect(colorButton).toHaveStyle({ 'background-color': 'blue' })
+
+  expect(colorButton).toHaveTextContent('Change to red')
 })
-
-test('button has correct initial text', () => {})
-
-test('button turns blue when clicked', () => {})
